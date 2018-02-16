8b7ed7ebfe4500834deb4540a9a1824c12021e43
sylhare <sylhareng@gmail.com>
2018-02-16 11:59:54 -0500

    Adding Zone and explosion tests

--- tests/tests_jasmine.js
+++ tests/tests_jasmine.js
@@ -1,10 +1,81 @@
-describe("A Board", function() {});
+describe("MINESWEEPER'S TESTS", function() {
 
-describe("A Zone of the Board", function() {});
+describe("The Board", function() {
 
-describe("An explosion", function() {});
+    describe("A Zone of the Board", function() {
 
-describe("A timer", function() {
+        it("can be created with a defined set of parameters", function() {
+            var zone = new Zone(1, 1, true, 20, 2);
+
+            expect(zone.x).toEqual(1);
+            expect(zone.y).toEqual(1);
+            expect(zone.mine).toEqual(true);
+            expect(zone.size).toEqual(20);
+            expect(zone.value).toEqual(2);
+        });
+
+        it("has a standard color by default", function(){
+            var zone = new Zone();
+
+            expect(zone.color).toEqual(colorDefault);
+            expect(colorDefault !== colorUnveiled).toBeTruthy();
+        });
+
+        it("is not flagged or unveiled when created", function(){
+            var zone = new Zone();
+
+            expect(zone.isUnveiled).toBeFalsy();
+            expect(zone.flag).toBeFalsy();
+        });
+
+        it("has")
+
+    });
+});
+
+describe("The explosion", function() {
+    it("is created from particle objects", function() {
+        var e = new Explosion();
+
+        expect(e.particles.length).toEqual(20);
+
+    });
+
+    describe("The explosion's particles", function() {
+        var e;
+
+        beforeEach(function() {
+            e = new Explosion(1, 1);
+        });
+
+        it("all start from the same defined x and y position", function () {
+            for (var p = 0; p < e.particles.length; p++) {
+                expect(e.particles[p].x).toEqual(1);
+                expect(e.particles[p].y).toEqual(1);
+            }
+        });
+
+        it("all have random colors", function(){
+            var p = 0;
+            var first_particle = e.particles[p];
+            var another_particle = first_particle;
+
+            for (var p = 1; p < e.particles.length; p++) {
+                var particle = e.particles[p];
+                expect(particle.color).toContain('#');
+
+                if (particle.color !== first_particle.color){
+                    another_particle = particle;
+                    break;
+                }
+            }
+
+            expect(first_particle.color !== another_particle).toBeTruthy();
+        });
+    });
+});
+
+describe("The timer", function() {
     var timer;
 
     beforeEach(function() {
@@ -37,6 +108,8 @@ describe("A timer", function() {
 
 
     });
+
+});
 
 });
 
