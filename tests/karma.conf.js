a5943a9d1f290052ed3ead2f586a7b35d6702d07
sylhare <sylhareng@gmail.com>
2018-02-20 15:03:55 -0500

    Fixing Travis Built #5

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+      browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
 
       // you can define custom flags
       customLaunchers: {
